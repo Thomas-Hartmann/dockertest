8705393a3fd124c65c01e20f13d28e4ec10ee423
Thomas Hartmann (THA - Adjunkt - Cphbusiness) <tha@cphbusiness.dk>
2016-08-24 16:03:32 +0200

    solved problem in HelloDockerWorld.java

--- HelloDockerWorld.java
+++ HelloDockerWorld.java
@@ -3,12 +3,15 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package dk.webtrade.maventestproject.entity;
 
 /**
  *
  * @author tha
  */
 public class HelloDockerWorld {
-    
+    public static void main(String[] args) {
+        System.out.println("Hello from this Docker build project");
+        System.out.println("Uploaded to github and autobuild on hub.docker.com");
+        System.out.println("then pulled from the hub into my droplet and run");
+    }
 }
